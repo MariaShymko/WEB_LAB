0ccb280dc0759d6759a7fb3e97416267faca50bd
Your Name <you@example.com>
2016-10-24 21:46:26 +0400

    new autotest for creation of static page

---
+++ tests/acceptance/Static page/creation_menu_for_static_pageCept.php
@@ -0,0 +1,20 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('perform actions and see result');
+$I->amOnPage('/backend');
+$I->fillField('.form-control.width-1.icon.user','olga');
+$I->fillField('.form-control.width-1.icon.lock','123123');
+$I->click('.btn.btn-primary.login-button');
+$I->waitForElement('#layout-sidenav li:nth-child(2)', 7);
+//$I->click('.nav-label');
+$I->click('#layout-sidenav li:nth-child(2) span');
+$I->click('#MenuItems-formMenuc7561fb6648597569008ffab41eb85e5580e477289fa8Items-item-list-items > a');
+$I->click('#MenuItems-formMenuc7561fb6648597569008ffab41eb85e5580e477289fa8Items-item-list-items > a');
+$I->click('#Form-formMenuc7561fb6648597569008ffab41eb85e5580e477289fa8ItemsMenuItem-field-title');
+$I->click('#select2-Form-formMenuc7561fb6648597569008ffab41eb85e5580e477289fa8ItemsMenuItem-field-type-container');
+$I->click('#Form-formMenuc7561fb6648597569008ffab41eb85e5580e477289fa8ItemsMenuItem-field-type-group > span');
+$I->click('#Form-formMenuc7561fb6648597569008ffab41eb85e5580e477289fa8ItemsMenuItem-field-reference-group > span > span.selection > span');
+$I->click('#select2-Form-formMenuc7561fb6648597569008ffab41eb85e5580e477289fa8ItemsMenuItem-field-reference-container');
+$I->click('body > div.control-popup.modal.fade.in > div > div > div.modal-footer > button.btn.btn-primary');
+$I->click('#Form-formMenuc7561fb6648597569008ffab41eb85e5580e477289fa8-field-toolbar-group > div > a');
+$I->see('вава', '#MenuItems-formMenuc7561fb6648597569008ffab41eb85e5580e477289fa8Items-item-list-items > ol > li:nth-child(7) > div > a > span.title');
