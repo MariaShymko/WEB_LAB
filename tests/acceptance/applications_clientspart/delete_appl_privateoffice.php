f252397f48c046b028b85df1de5d115b8d26a2aa
teacher <a@a.com>
2016-10-26 20:58:05 +0400

    corection

--- tests/acceptance/applications_clientspart/delete_appl_privateoffice.php
+++ tests/acceptance/applications_clientspart/delete_appl_privateoffice.php
@@ -1,4 +1,5 @@
 <?php 
+use Step\Acceptance\Login as AcceptanceTester;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('verify possibility of deliting work in private office');
 $I->haveInDatabase('lovata_contest_works', array (
@@ -8,8 +9,7 @@ $I->haveInDatabase('lovata_contest_works', array (
 	 'updated_at'=>'20окт.2016г.18.40' 
 	 'on_site'=>'1');	 
 $I->haveInDatabase('lovata_buddies_category', array ('name'=>'Профессиональная вертикаль',);
-$I->amOnPage('/');
-$I->see( 'aaa','.header-sign__login');
+$I->loginAsUser();
 $I->click('.header-sign__login');
 $I->see('.header-login__block-inside');
 $I->click('.login-nav__link');
