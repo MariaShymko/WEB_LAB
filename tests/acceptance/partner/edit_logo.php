3050f9c16430f9899af9f820c2e76192608a9f40
Your Name <you@example.com>
2016-10-26 21:36:50 +0300

    добавление исправленных

--- tests/acceptance/partner/edit_logo.php
+++ tests/acceptance/partner/edit_logo.php
@@ -1,17 +1,17 @@
 <?php
 use Step\Acceptance\Login as AcceptanceTester;
 $I = new AcceptanceTester ($scenario);
-$I->wantTo ('create logo');
+$I->wantTo ('edit logo');
 $I->loginAsManager ();
 
 $I->click('.nav-label');
 $I->click('.icon-users');
 $I->click('.btn.btn-primary.oc-icon-plus');
 $I->fillField('.form-control',  "name");
-$I->attachFile('.upload-button-icon.oc-icon-upload',  'dog.jpg');
+$I->attachFile('.upload-button-icon.oc-icon-upload',  'dog2.jpg');
 $I->click('.btn.btn-primary');
 
 $I->see('.list-cell-index-1.list-cell-name-name.list-cell-type-text   ');
 $I->click('.list-cell-index-1.list-cell-name-name.list-cell-type-text  ');
 $I->see('name', '.form-control');   
-$I->see('dog.jpg', '.upload-button-icon.oc-icon-upload'); 
+$I->see('dog2.jpg', '..upload-button-icon.oc-icon-upload'); 
