3b720834d045b7b7a145278b3e657f00cc235823
teacher <a@a.com>
2016-10-26 20:44:55 +0400

    val

--- tests/acceptance/applications_clientspart/creating_appl_clientpart_privateperson.php
+++ tests/acceptance/applications_clientspart/creating_appl_clientpart_privateperson.php
@@ -3,11 +3,11 @@ use Step\Acceptance\Login as AcceptanceTester;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('verify possibility of creating application by private person');
 $I->haveInDatabase('lovata_contest_works', array (
-     'name'=>'Name1',
-     'product'=>'Product1',
-     'created_at'=>'2016-10-20 18:40:00',
-	 'updated_at'=>'2016-10-20 18:40:00',
-	 'on_site'=>'1');	 
+    'name'=>'Name1',
+    'product'=>'Product1',
+    'created_at'=>'2016-10-20 18:40:00',
+    'updated_at'=>'2016-10-20 18:40:00',
+    'on_site'=>'1');	 
 $I->haveInDatabase('lovata_buddies_category', array ('name'=>'Профессиональная вертикаль',);
 $I->loginAsUser();
 $I->click('.invite-promo__participatebtn._action');
