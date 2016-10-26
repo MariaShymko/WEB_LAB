6bb51f557f73328db9df0b6f78009175156bcfce
teacher <a@a.com>
2016-10-26 21:04:25 +0400

    correction

--- tests/acceptance/applications_clientspart/edditing_appl_privateoffice.php
+++ tests/acceptance/applications_clientspart/edditing_appl_privateoffice.php
@@ -3,11 +3,11 @@ use Step\Acceptance\Login as AcceptanceTester;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('verify possibility of edditing work in private office');
 $I->haveInDatabase('lovata_contest_works', array (
-     'name'=>'Name1',
-	 'product'=>'Product1',
-	 'created_at'=>'20окт.2016г.18.40',	 
-	 'updated_at'=>'20окт.2016г.18.40' 
-	 'on_site'=>'1');	 
+    'name'=>'Name1',
+    'product'=>'Product1',
+    'created_at'=>'2016-10-20 18:40:00',
+    'updated_at'=>'2016-10-20 18:40:00',
+    'on_site'=>'1');	 
 $I->haveInDatabase('lovata_buddies_category', array ('name'=>'Профессиональная вертикаль',);
 $I->loginAsUser();
 $I->click('.header-sign__login');
